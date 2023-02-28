0b76f1152dd9a8a32ca2116514c177cdeb59b419
Rumi <car3@torder.co.kr>
2023-02-28 18:07:26 +0900

    기능:추가:수정: 테스트 프로젝트 1,2

--- testtest2/index.js
+++ testtest2/index.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const app = express();
 const server = require("http").createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server);
-const port = process.env.PORT || 3000;
+const socketIo = require("socket.io");
+const io = new socketIo(server);
+const port = process.env.PORT || 3001;
 const { createClient } = require("redis");
 const adapter = require("socket.io-redis");
 
@@ -42,18 +42,33 @@ const settingRedisClientList = () => {
 };
 
 io.on("connection", (socket) => {
-  const storeCode = socket.handshake.query.storeCode;
 
+  io.adapter(redisClients[0]).on("connection", (data)=> {
+
+  })
+
+  io.adapter(redisClients[1]).on("connection", (data)=> {
+    console.log(data)
+  })
+
+
+  const storeCode = socket.handshake.query.storeCode;
   if (storeCode) {
     switch (storeCode) {
       case "TEST_TPAY_003":
-        console.log(1,socket.id)
-        io.adapter(redisClients[0]).emit("storeCode", 'hello')
-          console.log(2)
+        // console.log(storeCode);
+          console.log(1,socket.id)
+        io.adapter(redisClients[0]).on("storeCode", (data)=> {
+          console.log(data)
+        })
+
+
         break;
       case "TEST_TPAY_001":
         io.adapter(redisClients[1]).emit("storeCode", storeCode);
         break;
     }
   }
+
+
 });
